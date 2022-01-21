5482051ca2181ea9a2457c4112320fa777289c6d
Hawa <hawa.francis@gmail.com>
2022-01-21 17:38:08 -0500

    work Page

--- src/pages/Lazy-Mixology.js
+++ src/pages/Lazy-Mixology.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 function Mixology (){
 <h1>hello</h1>
--- src/pages/work.js
+++ src/pages/work.js
@@ -5,81 +5,79 @@ import {Button, Card, Icon, CardTitle, Row, Col} from 'react-materialize';
 import Img1 from '../images/mixology-home.jpg';
 import Img2 from '../images/pet-home.jpg';
 import Img3 from '../images/events-home.jpg';
-import Page from './Lazy-Mixology';
+import PageSrc from './Lazy-Mixology';
 
 
 
 
-function Work (){
-
-
-    let data =[
-        {
-            id: 1,
-            imgSrc: Img1,
-            Button:"Lazy Mixology",
-            Page:'./Lazy-Mixology.js'
-        },
-        {
-            id: 2,
-            imgSrc: Img2,
-            Page:"Pet App",
-        },
-        {
-            id: 3,
-            imgSrc: Img3,
-            Page:"Events R Us"
-        },
-        // {
-        //     id: 4,
-        //     imgSrc: Img4,
-        // },
-        // {
-        //     id: 5,
-        //     imgSrc: Img5,
-        // },
-        // {
-        //     id: 6,
-        //     imgSrc: Img6,
-        // },
-        // {
-        //     id: 6,
-        //     imgSrc: Img7,
-        // },
-    ]
-    const [model, setModel] = useState(false);
-    const [tempimgSrc, setTempImgSrc] = useState('');
-
-    const getImg = (imgSrc) =>{
-        setTempImgSrc(imgSrc);
-        setModel(true);
-    }
-    return(
-        <>
-        <div ClassName = {model? "model open" : "model"}>
-            <img src={tempimgSrc} />
-            {/* <CloseIcon /> */}
-            {/* <i class="material-icons">close</i> */}
-        </div>
-        <div className="gallery">
-            {data.map((item, index)=>{
-                return(
-                    <div className="pics" key={index} onClick={() => (item.imgSrc)}>
-                        <img src={item.imgSrc} style={{width: '100%', height:'100%'}}/>
-                        <Button
-        href="/Lazy-Mixology" target="_blank"
-        node="a"
-        waves="light"
-        className="bio-button grey darken-4 valign-center"
-      >
-        Learn More
-      </Button>
-                    </div>
-                )
-            })}
-        </div>
-        </>
-    )
-}
-
-export default Work
+function About() {
+    return (
+      <Row className= "work-page">
+    <Col
+      m={6}
+      s={6}
+      className="about-card"
+    >
+       <Card className="about-image"
+       
+      //  closeIcon={<Icon>close</Icon>}
+       header={<CardTitle image={Img1}></CardTitle>}
+      //  revealIcon={<Icon>{Img8}</Icon>}
+     >
+         <Button
+          href="mailto:hawa.francis@gmail.com" target="_blank"
+          node="a"
+          waves="light"
+          className="bio-button grey darken-4 valign-center"
+        >
+          Contact Me
+        </Button>
+          </Card>
+    </Col>
+    <Col
+      m={6}
+      s={6}
+      className="about-card"
+    >
+       <Card className="about-image"
+       
+      //  closeIcon={<Icon>close</Icon>}
+       header={<CardTitle image={Img2}></CardTitle>}
+      //  revealIcon={<Icon>{Img8}</Icon>}
+     >
+         <Button
+          href="mailto:hawa.francis@gmail.com" target="_blank"
+          node="a"
+          waves="light"
+          className="bio-button grey darken-4 valign-center"
+        >
+          Contact Me
+        </Button>
+          </Card>
+    </Col>
+    <Col
+      m={6}
+      s={6}
+      className="about-card"
+    >
+       <Card className="about-image"
+       
+      //  closeIcon={<Icon>close</Icon>}
+       header={<CardTitle image={Img3}></CardTitle>}
+      //  revealIcon={<Icon>{Img8}</Icon>}
+     >
+         <Button
+          href="mailto:hawa.francis@gmail.com" target="_blank"
+          node="a"
+          waves="light"
+          className="bio-button grey darken-4 valign-center"
+        >
+          Contact Me
+        </Button>
+          </Card>
+    </Col>
+  </Row>      
+    );
+  }
+  
+  export default About;
